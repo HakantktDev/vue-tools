c066360543686b6a6a4c2a562c9f29880a239a9d
HakantktDev <hakantokatdev@gmail.com>
2024-03-11 10:21:41 +0300

    custom-components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script>
+import BaseCounter from './components/BaseCounter.vue'
 export default {
+  components: {
+    BaseCounter
+  },
   data: () => ({
-    count: 10,
-    counterTitle: 'Counter Standart',
-    incrementAmount: 8,
     message: 'Hello it works',
     listOfNumbers: [
       { name: 1, id: '1', list: [1, 2, 3] },
@@ -12,47 +13,12 @@ export default {
       { name: 4, id: '4', list: [1, 2, 3] },
       { name: 5, id: '5', list: [1, 2, 3] }
     ]
-  }),
-  computed: {
-    displayTitle() {
-      if (this.count > 20) {
-        return 'Counter Standard - Very Long'
-      } else {
-        return 'Counter Standard'
-      }
-    },
-    optimizedIncrementAmount() {
-      return this.displayTitle.length * this.incrementAmount
-    }
-  },
-  methods: {
-    incrementCount(newAmount, event) {
-      console.log(newAmount)
-      console.log(event)
-      this.count += this.optimizedIncrementAmount
-    }
-  },
-  watch: {
-    count(newValue) {
-      console.log(newValue)
-      if (newValue > 20) {
-        this.counterTitle += ' Very Long'
-      }
-    }
-  }
+  })
 }
 </script>
 
 <template>
-  <h1>{{ displayTitle }}</h1>
-  <p :data-increment-by="incrementAmount">{{ count }}</p>
-  <button @click="incrementCount(10, $event)">Increment Count</button>
-  <h1>{{ incrementAmount }}</h1>
-  <p>{{ optimizedIncrementAmount }}</p>
-  <div>
-    <label for="incrementAmount">Increment by:</label>
-    <input type="number" v-model="incrementAmount" />
-  </div>
+  <BaseCounter />
   <hr />
   <p v-if="message.length % 2 === 0">Even:{{ message.toUpperCase() }}</p>
   <p v-else>Odd:{{ message }}</p>
@@ -65,3 +31,4 @@ export default {
     </li>
   </ul>
 </template>
+./components/BaseCounter.vue
